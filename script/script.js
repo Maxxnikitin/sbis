6a4d5c16de0853c0ebe7bc706e8b325348cc58f8
Max Nikitin <maxxnikitin90@yandex.ru>
2020-06-08 15:46:31 +0300

    add adaptation and header-menu

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
 const buttonClientsOpened = document.querySelector('.clients__button');
+const buttonMenu = document.querySelector('.menu__button');
 const clientsHidden = document.querySelector('.clients__hidden');
+const headerMenu = document.querySelector('.header-menu');
 
-function openClientsReviews () {
+function changeClientsReviews () {
   clientsHidden.classList.toggle('clients__hidden_opened');
 }
 
-buttonClientsOpened.addEventListener('click', openClientsReviews);
+function changeHeaderMenu () {
+  headerMenu.classList.toggle('header-menu_opened');
+}
+
+buttonClientsOpened.addEventListener('click', changeClientsReviews);
+buttonMenu.addEventListener('click', changeHeaderMenu);
